3130a38a18d0370d12af2845e054efd24fdc4edb
Gulistan Saglam <gulistansglm@gmail.com>
2024-03-10 02:31:30 +0300

    first commit

---
+++ src/Widgets.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Widgets.css'
+import InfoIcon from '@mui/icons-material/Info';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+
+function Widgets() {
+
+    const newsArticle = (heading, subtitle) => (
+        <div className='widgets__article'>
+            <div className='widgets__articleLeft'>
+                <FiberManualRecordIcon />
+            </div>
+            <div className='widgets__articleRight'>
+                <h4>{heading}</h4>
+                <p>{subtitle}</p>
+            </div>
+        </div>
+    )
+
+    return (
+        <div className='widgets'>
+            <div className='widgets__header'>
+                <h2>LinkedIn News</h2>
+                <InfoIcon />
+            </div>
+            {newsArticle("Jung Kook is back", "Top news - 9099 readers")}
+            {newsArticle("Felix is very beautiful", "Top news - 9099 readers")}
+            {newsArticle("Gulistan needs money", "Top news - 9099 readers")}
+        </div>
+    )
+}
+
+export default Widgets
